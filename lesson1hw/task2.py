8ba3b862225e33b0c64199af9dfe1473f9595dd1
kikoooiemama <jojoka17@yandex.ru>
2021-11-22 02:36:18 +0300

    change token

--- lesson1hw/task2.py
+++ lesson1hw/task2.py
@@ -4,7 +4,7 @@
 import requests
 
 user_id = 'kikoooiemama'
-access_token='2821b480390f4e5cbda80e36caa90ece362a225f40d53cdc1d95823ec1bc39a533930b59d543cb9f1defc'
+access_token='token'
 version = '5.131'
 url = 'https://api.vk.com/method/groups.get'
 
